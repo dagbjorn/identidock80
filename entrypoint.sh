0314b4e1180fa55f9e85b15d80d1c5bccd12b2f0
Dagbjorn Nogva <dagbjorn.nogva@broadpark.no>
2016-03-28 20:44:36 +0200

    First commit

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# -e  Exit immediately if a command exits with a non-zero status.
+set -e
+
+if [ "$ENV" = "dev" ]; then
+    echo "Running Development Server"
+    exec python "identidock.py"
+elif [ "$ENV" = "unit" ]; then
+    echo "Running Unit Tests"
+    exec python "tests.py"
+else
+    echo "Running Production Server"
+    exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
+                --callable app --stats 0.0.0.0:9191
+fi
